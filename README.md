cefc4d18e7acc5ac8b680a0798522350f5bbd87d
stardev8933 <stardev8933@gmail.com>
2022-10-31 23:34:45 +0900

    Step02. Spring run test

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # community
 책-처음 배우는 스프링 부트 2 프로젝트 - mac, intellij 버전
+1.jetBrains install 
+2.intellij community install
+3.spring initialize download
+
+# branch master - setting
+1.Spring Project setting
+
+# branch step02 - spring run test
+1.Spring run test
+2.Gradle install(Auto)
+3.Gradle Multi Project Setting
+ - add root project : settings.gradle -> rootProject.name = 'demo'
+ - root > new > module -> Gradle -> Java -> next
--- src/main/java/com/community/CommunityApplication.java
+++ src/main/java/com/community/CommunityApplication.java
@@ -2,7 +2,10 @@ package com.community;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 @SpringBootApplication
 public class CommunityApplication {
 
@@ -10,4 +13,8 @@ public class CommunityApplication {
 		SpringApplication.run(CommunityApplication.class, args);
 	}
 
+	@GetMapping
+	public String HelloWorld() {
+		return "Hello World";
+	}
 }
